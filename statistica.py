f2e3c06e318dea36dd5e2cf3212f6ddc6fdeea54
Stefano Pontellini <stefanopontellini@gmail.com>
2025-01-28 15:39:47 +0100

    Update statistica.py

--- statistica.py
+++ statistica.py
@@ -1,6 +1,6 @@
 def main() :
     votazione = []
-    print("Con la seguente funzione si può dare un giudizio (da 0 a 10) per valutare l'indice di gradimento del film (premere un qualsiasi tasto per terminare)")
+    print("Con la seguente funzione si può dare un giudizio (da 0 a 10) in base all'indice di gradimento del film (premere un qualsiasi tasto per terminare)")
     voto = input()
     while isNumberValid(voto) : 
         votazione.append(int(voto))
@@ -9,14 +9,14 @@ def main() :
     if lunghezzaVettore > 2 :
         votazione.remove(min(votazione))
         votazione.remove(max(votazione))
-        print("La media delle votazioni è di: %.2f" %(sum(votazione) / len(votazione)))
+        print("L'indice di gradimento del film è di %.2f" %(sum(votazione) / len(votazione)), "punti")
     elif lunghezzaVettore > 0 & lunghezzaVettore < 2 :
         print("Non ho abbastanza valori per poter stimare una media corretta")
     else :
         print("I dati inseriti non sono validi")
 
 def isNumberValid(stringaNumero) :
-    if len(stringaNumero) > 0 :
+    if (len(stringaNumero)) > 0 :
         if stringaNumero.isdigit() :
             if int(stringaNumero) < 11 :
                 return True
